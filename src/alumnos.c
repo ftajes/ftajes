69d2933bbdf533e07d98b6c6f4bef9acf7a43318
Fernando <f.tajes@gmail.com>
2020-12-19 17:12:46 -0300

    Se agregan los cambios del alumno TAJES, Fernando

--- src/alumnos.c
+++ src/alumnos.c
@@ -25,14 +25,14 @@
 
 /*=====[Definitions of public global variables]==============================*/
 
-static const struct alumno_s ESTEBAN_VOLENTINI = {
-    .apellidos = "VOLENTINI",
-    .nombres = "Esteban Daniel",
-    .documento = "23.517.968",
+static const struct alumno_s FERNANDO_TAJES = {
+    .apellidos = "TAJES",
+    .nombres = "Fernando",
+    .documento = "3.864.221-4",
 };
 
 const alumno_t ALUMNOS[] = {
-    &ESTEBAN_VOLENTINI,
+    &FERNANDO_TAJES,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
